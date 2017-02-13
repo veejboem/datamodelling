971307940a5da5e32d49a17a2772285fb7aa5c75
Valerie Boem <vboem@gmail.com>
2017-02-13 16:38:06 -0500

    Worked on data models for assignments 1-4

---
+++ dr_appointment/db/migrate/20170203204906_create_appointments.rb
@@ -0,0 +1,12 @@
+class CreateAppointments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :appointments do |t|
+      t.string :time
+      t.date :date
+      t.integer :doctor_id
+      t.integer :patient_id
+
+      t.timestamps
+    end
+  end
+end
